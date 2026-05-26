2cb6c599bc7afce3c435fd4894f59b5792eba677
Daniel ferreira dos santos <daniel.inazumaeleven@gmail.com>
2026-05-26 16:55:38 -0300

    Update gestao_estoque_Daniel_Ferreira_dos_Santos.py

--- gestao_estoque_Daniel_Ferreira_dos_Santos.py
+++ gestao_estoque_Daniel_Ferreira_dos_Santos.py
@@ -15,7 +15,7 @@ print("=" * 50)
 def cadastrar_produto(nome, categoria, preço, quantidade):
     """Registra novo produto no sistema"""
     
-    #Permite modificar a variável golobal proximo_id
+    #Permite modificar a variável global proximo_id
     global proximo_id
     
     #criação do dicionário do produto
@@ -37,3 +37,55 @@ def cadastrar_produto(nome, categoria, preço, quantidade):
     #incrementa o ID para o próximo produto
     proximo_id += 1
 
+print("=" * 50)
+#FUNÇÃO PARA REGISTRAR ENTRADA DE PRODUTOS
+print("=" * 50)
+def registrar_entrada(produto_id, quantidade):
+    """adiciona itens ao estoque"""
+    
+    #verifica se o produto existe no estoque
+    if produto_id in estoque:
+        
+        #Soma a quantidade informada ao estoque atual
+        estoque[produto_id]["quantidade"] += quantidade
+
+        #mensagem de sucesso
+        print("\n Entrada registrada com sucesso!")
+    else:
+        #Mensagem caso o produto não exista
+        print("\n produto não encontrado!")
+
+print("=" * 50)
+#FUNÇÃO PARA REGISTRAR A SAÍDA DE PRODUTOS
+print("=" * 50)
+def registrar_saida(produto_id, quantidade):
+    """Remove itens do estoque"""
+    
+    #Verifica se o produto existe
+    if produto_id in estoque:
+        
+        #Armazena a quantidade atual do produto
+        quantidade atual = estoque[produto_id]["quantidade"]
+        
+        #Verifica se há quantidade suficiente no estoque
+        if quantidade <= quantidade_atual:
+            #Subtrai a quantidade do estoque
+            estoque[produto_id]["quantidade"] -= quantidade
+            #mensagem de sucesso
+            print("\n saída registrada com sucesso!")
+         
+        else:
+            #mensagem de erro caso não tenha estoque suficiente
+            print("\n Estoque insuficiente!")
+print("=" * 50)
+#FUNÇÃO PARA CONSULTAR PRODUTOS
+print("=" * 50)
+def consultar_estoque(produto_id):
+    """Verifica saldo atual do produto"""
+
+    #Verifica se o produto existe
+    if produto_id ind estoque:
+        #Armazena os dados do produto em uma variável
+        produto = estoque[produto_id]
+    
+        
